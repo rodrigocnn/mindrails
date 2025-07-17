07dae043fce75c90b1adcadc362d4645753820ea
Rodrigo César <rodrigocnn@gmail.com>
2025-07-17 15:55:44 -0300

    feat(routes) add resources to pacientes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :pacientes
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   scope "/admin" do
     get "dashboard", to: "dashboard#index"
+    resources :pacientes
   end
 end
 
