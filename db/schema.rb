c94d0e740dcf72afe10f366554114069c9c4d702
Rodrigo César <rodrigocnn@gmail.com>
2025-07-22 11:31:12 -0300

    feat:(schema) create table local_de_atendimentos

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_17_185840) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_22_115040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "local_de_atendimentos", force: :cascade do |t|
+    t.string "nome"
+    t.string "endereco"
+    t.string "bairro"
+    t.string "cidade"
+    t.string "estado"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pacientes", force: :cascade do |t|
     t.string "nome"
